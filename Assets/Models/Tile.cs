cdfd524fbb30513b9658c4c3f53bb4c68aef8dff
Tomáš Hořovský <horovtom@fel.cvut.cz>
2017-08-04 15:01:34 +0200

    Pt1
    Creating basic class structure

---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile {
+	
+	enum TileType {
+		Empty,
+		Floor
+	};
+
+	TileType type = TileType.Empty;
+
+	LooseObject looseObject;
+	InstalledObject installedObject;
+
+	World world;
+	int x, y;
+
+	public Tile(World world, int x, int y) {
+		this.world = world;
+		this.x = x;
+		this.y = y;
+	}
+
+}
