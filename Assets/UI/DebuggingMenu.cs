dceb393a93bdd0b4f592b4997ccf7ac020d0606f
Tomáš Hořovský <horovtom@fel.cvut.cz>
2017-08-13 18:05:47 +0200

    Moving Debugging buttons to separate GO
    Deleting buttons for Displaying Pathfinding and building basic map and moving functionality to new DebuggingMenu GameObject

---
+++ Assets/UI/DebuggingMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebuggingMenu : MonoBehaviour {
+
+	public BuildModeController buildModeController; 
+
+	// Use this for initialization
+	void Start () {
+		//buildModeController = (BuildModeController)(GameObject.Find("BuildModeController"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CreatePathfindingExample() {
+		buildModeController.DoPathfindingTest();
+	}
+
+	public void ShowPathfindingDebuggingInfo() {
+		buildModeController.DisplayPathfindingDebug();
+	}
+}
---
+++ Assets/UI/Editor/DebuggingMenuEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(DebuggingMenu))]
+public class DebuggingMenuEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+		if (GUILayout.Button("Create Pathfinding Example.")) {
+			((DebuggingMenu)target).CreatePathfindingExample();
+		}
+
+		if (GUILayout.Button("Show Pathfinding debugging info.")) {
+			((DebuggingMenu)target).ShowPathfindingDebuggingInfo();
+		}
+	}
+}
